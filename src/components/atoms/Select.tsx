a77deaca3c7705e105f000f67553013df47bd7d5
NxtPerfect <alakaxan@protonmail.com>
2025-06-05 07:10:23 +0200

    feat: changed select dropdown animation
    fix: added overflow scroll to select dropdown

--- src/components/atoms/Select.tsx
+++ src/components/atoms/Select.tsx
@@ -28,14 +28,13 @@ export default function Select({ defaultItem = "default", items = [{ text: "one"
         className={`relative z-10 bg-black dark:bg-white dark:text-black rounded-xl
         px-4 flex flex-row cursor-pointer gap-2`}
         onClick={() => setIsOpened((cur) => !cur)}
-        whileTap={{ scale: 0.95 }}
       >
         <li className="list-none">
           {selectedItemIndex < 0 ? defaultItem : items[selectedItemIndex].text}
         </li>
         <span
           className={`cursor-pointer ${isOpened ? "rotate-90" : ""}
-duration-100`}>
+motion-safe:duration-100`}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
           </svg>
@@ -44,23 +43,27 @@ duration-100`}>
       <AnimatePresence initial={false}>
         {isOpened && <motion.ul
           key="dropdown"
-          className={`relative bg-black dark:bg-white dark:text-black rounded-xl
-p-2 gap-2 flex flex-col z-0`}
-          initial={{ y: -24 }}
-          animate={{ y: 0 }}
-          exit={{ y: -24 }}
+          className={`relative bg-black dark:bg-white dark:text-black rounded-b-xl
+z-0 mt-[-.5lh] max-h-[7lh]`}
+          initial={{ height: 0, overflow: "hidden" }}
+          animate={{ height: "auto", overflow: "scroll" }}
+          exit={{ height: 0, overflow: "hidden" }}
           transition={{ duration: 0.1 }}
         >
-          {selectedItemIndex !== -1 && <li className="cursor-pointer hover:text-neutral-500"
-            onClick={handleSelect}>{defaultItem}</li>}
-          {isOpened && items.filter((_, index) => index !== selectedItemIndex).map((item) => {
-            return <li
-              className="cursor-pointer hover:text-neutral-500"
-              onClick={handleSelect}
-            >
-              {item.text}
-            </li>
-          })}
+          <div className="p-2 py-4 gap-2 flex flex-col">
+            {selectedItemIndex !== -1 && <li className="cursor-pointer hover:text-neutral-500"
+              onClick={handleSelect}>{defaultItem}</li>}
+            {isOpened && items.filter((_, index) => index !== selectedItemIndex).map((item) => {
+              return (
+                <li
+                  className="cursor-pointer hover:text-neutral-500"
+                  onClick={handleSelect}
+                >
+                  {item.text}
+                </li>
+              )
+            })}
+          </div>
         </motion.ul>
         }
       </AnimatePresence>
