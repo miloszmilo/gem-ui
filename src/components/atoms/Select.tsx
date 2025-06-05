4bb97527c7bfc210807b85d210f4c90ea7f4d3f2
NxtPerfect <alakaxan@protonmail.com>
2025-06-05 06:50:25 +0200

    feat: added animation to select dropdown
    feat: added chevron to select

--- src/components/atoms/Select.tsx
+++ src/components/atoms/Select.tsx
@@ -23,29 +23,33 @@ export default function Select({ defaultItem = "default", items = [{ text: "one"
     onSelect(index, items[index]);
   }
   return (
-    <div>
+    <div className="relative inline-block">
       <motion.button
-        className={`bg-black dark:bg-white dark:text-black rounded-full
-        px-4 flex flex-row gap-2 cursor-pointer`}
+        className={`relative z-10 bg-black dark:bg-white dark:text-black rounded-xl
+        px-4 flex flex-row cursor-pointer gap-2`}
         onClick={() => setIsOpened((cur) => !cur)}
         whileTap={{ scale: 0.95 }}
       >
         <li className="list-none">
           {selectedItemIndex < 0 ? defaultItem : items[selectedItemIndex].text}
         </li>
-        <span className="cursor-pointer">
-          {isOpened ? "<" : "V"}
+        <span
+          className={`cursor-pointer ${isOpened ? "rotate-90" : ""}
+duration-100`}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          </svg>
         </span>
       </motion.button>
       <AnimatePresence initial={false}>
         {isOpened && <motion.ul
           key="dropdown"
-          className={`bg-black dark:bg-white dark:text-black rounded-xl
-p-2 gap-2 flex flex-col`}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.15 }}
+          className={`relative bg-black dark:bg-white dark:text-black rounded-xl
+p-2 gap-2 flex flex-col z-0`}
+          initial={{ y: -24 }}
+          animate={{ y: 0 }}
+          exit={{ y: -24 }}
+          transition={{ duration: 0.1 }}
         >
           {selectedItemIndex !== -1 && <li className="cursor-pointer hover:text-neutral-500"
             onClick={handleSelect}>{defaultItem}</li>}
